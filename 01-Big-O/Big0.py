a2c27f46be45d38df7f9cd0759b293bbbefa4b7e
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-16 22:59:00 +0000

    Added Big-O

---
+++ 01-Big-O/Big0.py
@@ -0,0 +1,30 @@
+# def print_items(n):
+#   for i in range(n):
+#     print(i)
+
+#   for j in range(n):
+#     print(j)
+
+# print_items(10)
+
+# def print_items(n):
+#   for i in range(n):
+#       for j in range(n):
+#         print(i, j)
+
+# print_items(10)
+
+# def print_items(n):
+#   for i in range(n):
+#       for j in range(n):
+#         print(i, j)
+
+#   for k in range(n):
+#     print(k)
+
+# print_items(10)
+
+def add_items(n):
+  return n + n
+
+print(add_items(100))
