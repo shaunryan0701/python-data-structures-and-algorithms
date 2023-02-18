52389041eeea0d545c92255188e43345272ad43b
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-18 10:29:28 +0000

    Added Doubly Linked Lists

---
+++ 03-DoublyLinkedLists/DoublyLinkedLists.py
@@ -0,0 +1,17 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+		self.prev = None
+
+
+class DoublyLinkedList:
+	def __init__(self, value):
+		new_node = Node(value)
+		self.head = new_node
+		self.tail = new_node
+		self.length = 1
+
+
+my_doubly_linked_list = DoublyLinkedList(5)
+print(my_doubly_linked_list)
