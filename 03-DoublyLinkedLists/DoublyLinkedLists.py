3ed2282235eab194925a6fc08dcb08f2ac9f950f
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-19 16:58:01 +0000

    Added Doubly Linked Lists methods

--- 03-DoublyLinkedLists/DoublyLinkedLists.py
+++ 03-DoublyLinkedLists/DoublyLinkedLists.py
@@ -12,6 +12,22 @@ class DoublyLinkedList:
 		self.tail = new_node
 		self.length = 1
 
+	
+	def print_list(self):
+		temp = self.head
+		while temp is not None:
+			print (temp.value)
+			temp = temp.next
+
+	def append(self, value):
+		new_node = Node(value)
+		if self.head == None:
+			self.head = new_node
+			self.tail = new_node
+		new_node.prev = self.tail
+		self.tail.next = new_node
+		self.tail = new_node
 
 my_doubly_linked_list = DoublyLinkedList(5)
 print(my_doubly_linked_list)
+my_doubly_linked_list.print_list()
