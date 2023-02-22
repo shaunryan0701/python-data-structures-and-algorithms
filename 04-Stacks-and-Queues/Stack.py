4a999477d041ae43d6879e4626fe7270e565ab63
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-22 12:20:47 +0000

    Added methods for Stack class

--- 04-Stacks-and-Queues/Stack.py
+++ 04-Stacks-and-Queues/Stack.py
@@ -29,8 +29,25 @@ class Stack:
     return True
 
 
+  def pop(self):
+    if self.height == 0:
+      return None
+    temp = self.top
+    self.top = self.top.next
+    temp.next = None
+    self.height -= 1
+    return temp
+
+
 my_stack = Stack(3)
 my_stack.print_stack()
 
 my_stack.push(5)
+my_stack.print_stack()
+
+my_stack.push(8)
+my_stack.push(11)
+my_stack.print_stack()
+
+my_stack.pop()
 my_stack.print_stack()
