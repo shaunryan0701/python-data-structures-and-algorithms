729a42b25c421f94f30743fa0a1d573b7056e213
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-24 17:03:00 +0000

    Added constructor for Queues

--- 04-Stacks-and-Queues/Queue.py
+++ 04-Stacks-and-Queues/Queue.py
@@ -7,3 +7,16 @@ class Node:
 class Queue:
   def __init__(self,value):
     new_node = Node(value)
+    self.first = new_node
+    self.last = new_node
+    self.length = 1
+
+
+  def print_queue(self):
+    temp = self.first
+    while temp is not None:
+      print(temp.value)
+      temp = temp.next
+
+my_queue = Queue(4)
+my_queue.print_queue()
