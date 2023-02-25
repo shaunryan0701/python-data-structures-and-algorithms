25d2dad0445e614f136bfac1612f05245fef481d
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-02-25 18:08:09 +0000

    Added methods for Queues

--- 04-Stacks-and-Queues/Queue.py
+++ 04-Stacks-and-Queues/Queue.py
@@ -18,5 +18,20 @@ class Queue:
       print(temp.value)
       temp = temp.next
 
+
+  def enqueue(self, value):
+    new_node = Node(value)
+    if self.length == 0:
+      self.first = new_node
+      self.last = new_node
+    else:
+      self.last.next = new_node
+      self.last = new_node
+    self.length += 1
+
 my_queue = Queue(4)
+my_queue.print_queue()
+
+my_queue.enqueue(7)
+my_queue.enqueue(9)
 my_queue.print_queue()
