e2a5a3d1a01d90ec5342113b4a8d9b9a5430ee61
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-03-13 09:57:24 +0000

    Addded dequeue method to Queue class

--- 04-Stacks-and-Queues/Queue.py
+++ 04-Stacks-and-Queues/Queue.py
@@ -5,7 +5,7 @@ class Node:
 
   
 class Queue:
-  def __init__(self,value):
+  def __init__(self, value):
     new_node = Node(value)
     self.first = new_node
     self.last = new_node
@@ -29,11 +29,25 @@ class Queue:
       self.last = new_node
     self.length += 1
 
+  def dequeue(self):
+    if self.length == 0:
+      return None
+    
+    temp = self.first
+    if self.length == 1:
+      self.first = None
+      self.last = None
+    else:
+      self.first = self.first.next  
+      temp.next = None
+    self.length -= 1
+    return temp
+
 my_queue = Queue(4)
 my_queue.print_queue()
 
-
-
 my_queue.enqueue(7)
 my_queue.enqueue(9)
+my_queue.print_queue()
+my_queue.dequeue()
 my_queue.print_queue()
